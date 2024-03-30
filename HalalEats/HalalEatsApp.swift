05c84f1ece823f4b4dd133d9dbdc0c158ef90c1c
Abdullah Ridwan <53100755+abdullahridwan@users.noreply.github.com>
2024-03-30 12:38:08 -0400

    Initial Commit

---
+++ HalalEats/HalalEatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalalEatsApp.swift
+//  HalalEats
+//
+//  Created by Abdullah Ridwan on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct HalalEatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
